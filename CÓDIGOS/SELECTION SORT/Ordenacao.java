9955e83383c72f000829dd4beb3f7ec9a1ab9273
Carol <carolportellazanelato@gmail.com>
2026-07-07 00:43:41 -0300

    Add files via upload

---
+++ CÓDIGOS/SELECTION SORT/Ordenacao.java
@@ -0,0 +1,19 @@
+public class Ordenacao {
+
+    public static void ordena(int v[], int n) {
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (v[j] < v[min]) {
+                    min = j;
+                }
+            }
+
+            /* Troca v[min] e v[i] */
+            int x = v[min];
+            v[min] = v[i];
+            v[i] = x;
+        }
+    }
+}
