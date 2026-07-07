77f15531d26bd2b74926fd15ac8ba1dda47e3aa6
Carol <carolportellazanelato@gmail.com>
2026-07-07 00:27:45 -0300

    Add files via upload

---
+++ CÓDIGOS/CADEIA DE CARACTERES/ItemVoc.java
@@ -0,0 +1,49 @@
+
+interface Item{
+
+}
+
+public class ItemVoc implements Item {
+
+    private String palavra;
+    private int freq, ordem;
+    // outros componentes do registro
+
+    public ItemVoc(String palavra, int freq, int ordem) {
+        this.palavra = palavra;
+        this.freq = freq;
+        this.ordem = ordem;
+    }
+
+    public int compara(Item it) {
+        ItemVoc item = (ItemVoc) it;
+
+        if (this.freq < item.freq)
+            return 1;
+        else if (this.freq > item.freq)
+            return -1;
+
+        return 0;
+    }
+
+    public void alteraChave(Object freq) {
+        Integer ch = (Integer) freq;
+        this.freq = ch.intValue();
+    }
+
+    public Object recuperaChave() {
+        return new Integer(this.freq);
+    }
+
+    public void alteraOrdem(int ordem) {
+        this.ordem = ordem;
+    }
+
+    public int recuperaOrdem() {
+        return this.ordem;
+    }
+
+    public String palavra() {
+        return this.palavra;
+    }
+}
